2249c86716516392580e6893b01415cfe048778a
Jose Silva <jhheidrich@gmail.com>
2025-05-28 09:12:01 -0300

    alterada ami

--- terraform/mainf.tf
+++ terraform/mainf.tf
@@ -7,8 +7,25 @@ resource "aws_key_pair" "key_pair" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
 resource "aws_instance" "instance" {
-  ami                    = "ami-0c55b159cbfafe1f0" # Ubuntu Server 22.04 LTS (verifique a região)
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = "${var.project_name}-key"
   subnet_id              = module.vpc.public_subnets[0]
