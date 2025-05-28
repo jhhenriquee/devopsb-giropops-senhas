5acfe68e04b53a197036103d39529707cd297f83
Jose Silva <jhheidrich@gmail.com>
2025-05-28 10:20:17 -0300

    ajustado user data

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -4,8 +4,6 @@ set -eux
 apt-get update -y
 apt-get install -y git curl
 curl -fsSL https://get.docker.com | bash
-apt-get install -y uidmap
-dockerd-rootless-setuptool.sh install
 
 git clone https://github.com/jhhenriquee/devopsb-giropops-senhas.git /opt/giropops
 cd /opt/giropops
